d3ceae436f4f05d235debecb7a413e058219105a
nathan-rees <poohplop1@gmail.com>
2020-02-13 14:31:51 +0000

    setup test

--- src/test/java/imss/TestSetUp.java
+++ src/test/java/imss/TestSetUp.java
@@ -1,14 +1,19 @@
 package imss;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import com.qa.databases.SetUp;
 
 public class TestSetUp {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+		SetUp setup=new SetUp();
+		assertNotNull(setup);
 		
 	}
 
