ded911ffa16fa1a288688e6c003d36b54d4b30f1
nathan-rees <poohplop1@gmail.com>
2020-02-13 18:23:46 +0000

    test customer dao

--- src/test/java/imss/TestSetUp.java
+++ src/test/java/imss/TestSetUp.java
@@ -6,21 +6,18 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 import com.qa.databases.SetUp;
+import com.qa.databases.sql.MysqlCustomerDao;
 
 public class TestSetUp {
 
 	@Test
 	public void test() {
 		SetUp setup=new SetUp();
+		MysqlCustomerDao d=new MysqlCustomerDao("noobb");
 		assertNotNull(setup);
 		
 		
 	}
 	
-
-
-
-	
-
 
 }
