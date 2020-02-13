a52ea82f8ac9c28802bfc727b56220d8a93916a6
nathan-rees <poohplop1@gmail.com>
2020-02-13 14:28:52 +0000

    test

---
+++ src/test/java/imss/TestSetUp.java
@@ -0,0 +1,15 @@
+package imss;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSetUp {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}
